dc8867441c5f5d62c4e7160b7b013689a6d1fe02
twinbird827 <twinbird827@gmail.com>
2019-08-05 15:08:43 +0900

    [new] 正規表現の確認用プログラム

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = "id";
+            var regex = $@"(?<{key}>aa[\d]+)";
+            var txt = @"aa12346578sdfafasdaa123fsdaa456werew";
+            var value = Regex.Match(txt, regex).Groups[key].Value;
+            var values = Regex.Matches(txt, regex).OfType<Match>()
+                .Select(m => m.Groups[key].Value);
+
+            using (var log = new EventLog())
+            {
+                log.Source = typeof(Program).FullName;
+                log.WriteEntry(txt, EventLogEntryType.Error);
+            }
+
+            Console.WriteLine(string.Join(", ", values));
+            Console.ReadLine();
+        }
+    }
+}
